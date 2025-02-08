b38b64319eafde70a36efa141baacf5b7ba70759
abhishek 1199 <abhishekshaw1199@gmail.com>
2025-02-08 13:24:57 +0530

    1

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Movie from './Page/Movie';
+
+function App() {
+  return (
+    <div className="App">
+    <Movie/>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/Page/Movie.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Movie = () => {
+  return (
+    <div>
+      hii
+      
+    </div>
+  )
+}
+
+export default Movie
